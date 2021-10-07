c7b6a248f96331630c3d6099d69bc0b11bc1be1f
strainel <stephane.trainel@gmail.com>
2021-10-07 22:20:35 +0200

    change get for regbzh

--- scripts/sources/region-bretagne/get.sh
+++ scripts/sources/region-bretagne/get.sh
@@ -4,4 +4,4 @@
 # Récupération des données
 
 
-wget -r -q -nc -nd -nH --accept=xml -np  https://data.bretagne.bzh/api/datasets/1.0/decp-crb/alternative_exports/decp2021_regionbretagne_csv_xml
+wget -q -O decp2021_regionbretagne.xml https://data.bretagne.bzh/api/datasets/1.0/decp-crb/alternative_exports/decp2021_regionbretagne_csv_xml
