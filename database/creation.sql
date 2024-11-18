33589887656608a9f7205c6c041097f3076817ba
Your Name <you@example.com>
2024-11-18 14:34:51 +0100

    Added view for stats requests

--- database/creation.sql
+++ database/creation.sql
@@ -1,3 +1,4 @@
+DROP SEQUENCE IF EXISTS decp_report.s_account;
 DROP SEQUENCE IF EXISTS decp_report.s_session;
 DROP SEQUENCE IF EXISTS decp_report.s_report;
 DROP SEQUENCE IF EXISTS decp_report.s_step;
@@ -6,6 +7,7 @@ DROP SEQUENCE IF EXISTS decp_report.s_source;
 DROP SEQUENCE IF EXISTS decp_report.s_exclusion_type;
 
 CREATE SEQUENCE decp_report.s_session;
+CREATE SEQUENCE decp_report.s_account;
 CREATE SEQUENCE decp_report.s_report;
 CREATE SEQUENCE decp_report.s_step;
 CREATE SEQUENCE decp_report.s_file;
@@ -52,6 +54,16 @@ CREATE INDEX fk_report_source_id on decp_report.report (source_id);
 
 CREATE INDEX fk_report_file_id on decp_report.report (file_id);
 
+DROP TABLE IF EXISTS decp_report.account;
+
+CREATE TABLE decp_report.account (
+   account_id           INT8                 not null,
+   user_name            VARCHAR(256)         not null,
+   passwd               VARCHAR(256)         null,
+   active               BOOL                 null,
+   source_id            INT8                 null,
+   CONSTRAINT pk_account primary key (account_id)
+);
 
 DROP TABLE IF EXISTS decp_report.session;
 
@@ -129,4 +141,94 @@ ALTER TABLE decp_report.file
    ADD CONSTRAINT fk_file_source FOREIGN KEY (source_id)
       REFERENCES decp_report.source (source_id)
       ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE decp_report.account
+   ADD CONSTRAINT fk_account_source FOREIGN KEY (source_id)
+      REFERENCES decp_report.source (source_id)
+      ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+DROP VIEW decp_report.v_stats_global_by_session;
+
+CREATE OR REPLACE VIEW decp_report.v_stats_global_by_session AS 
+ SELECT s.begin_date,s.end_date, src."name", SUM(f.nb_marches) + SUM(f.nb_concessions) AS nb_records, SUM( nb_error) AS nb_errors
+ FROM decp_report."session" s 
+ INNER JOIN (
+ 	SELECT r.session_id,r.file_id,count(DISTINCT r.position) AS nb_error,f.nb_marches, f.nb_concessions
+ 	FROM decp_report.report r
+	 INNER JOIN decp_report.file f 
+	 ON f.file_id = r.file_id 
+ 	GROUP BY r.session_id,r.file_id,f.nb_marches ,f.nb_concessions
+ ) r
+ ON r.session_id = s.session_id 
+ INNER JOIN decp_report.file f 
+ ON f.file_id = r.file_id 
+ INNER JOIN decp_report."source" src 
+ ON src.source_id = f.source_id 
+ GROUP BY s.session_id,s.name,s.begin_date,s.end_date, src."name";
+ 
+DROP VIEW decp_report.v_stats_all;
+
+CREATE OR REPLACE VIEW decp_report.v_stats_all AS 
+SELECT s.name,s.source_id,r.session_id,
+	(SELECT end_date FROM decp_report."session" si WHERE si.session_id = r.session_id) AS session_date,
+	r.nb_errors,
+	r.nb_records,
+	100 * r.nb_errors / COALESCE(r.nb_records,NULL) AS per_errors
+FROM (
+	SELECT r.source_id,r.session_id,sum(DISTINCT file_id) AS nb_files,sum(nb_error) AS nb_errors, sum(nb_marches) + sum(nb_concessions) AS nb_records
+	FROM (
+	 	SELECT r.source_id,r.session_id,r.file_id,count(DISTINCT r.position) AS nb_error,f.nb_marches, f.nb_concessions
+	 	FROM decp_report.report r
+		INNER JOIN decp_report.file f 
+		ON f.file_id = r.file_id 
+		WHERE r.exclusion_type_id=3
+	 	GROUP BY r.source_id,r.session_id,r.file_id,f.nb_marches,f.nb_concessions
+	 	ORDER BY r.session_id
+	) r
+	GROUP BY r.source_id,r.session_id
+	ORDER BY r.session_id
+) r
+INNER JOIN decp_report.source s 
+ON s.source_id = r.source_id
+ORDER BY name;
+
+SELECT * FROM decp_report.v_stats_all;
+
+DROP VIEW decp_report.v_stats_global;
+
+CREATE OR REPLACE VIEW decp_report.v_stats_global AS
+SELECT session_id,
+	(SELECT end_date FROM decp_report."session" si WHERE si.session_id = r.session_id) AS session_date,
+	SUM(CASE WHEN source_id=1 THEN nb_records ELSE NULL END) AS nb_records_dematis,
+	SUM(CASE WHEN source_id=1 THEN nb_errors ELSE NULL END) AS nb_errors_dematis,
+	100 * SUM(CASE WHEN source_id=1 THEN nb_errors ELSE NULL END) /
+	COALESCE (SUM(CASE WHEN source_id=1 THEN nb_records ELSE NULL END),NULL) AS per_errors_dematis,
+	SUM(CASE WHEN source_id=310 THEN nb_records ELSE NULL END) AS nb_records_pes,
+	SUM(CASE WHEN source_id=310 THEN nb_errors ELSE NULL END) AS nb_errors_pes,
+	100 * SUM(CASE WHEN source_id=310 THEN nb_errors ELSE NULL END) / 
+	COALESCE (SUM(CASE WHEN source_id=310 THEN nb_records ELSE NULL END),NULL) AS per_errors_pes
+FROM (
+	SELECT s.name,s.source_id,r.session_id,r.nb_errors,r.nb_records
+	FROM (
+		SELECT r.source_id,r.session_id,sum(DISTINCT file_id) AS nb_files,sum(nb_error) AS nb_errors, sum(nb_marches) + sum(nb_concessions) AS nb_records
+		FROM (
+		 	SELECT r.source_id,r.session_id,r.file_id,count(DISTINCT r.position) AS nb_error,f.nb_marches, f.nb_concessions
+		 	FROM decp_report.report r
+			INNER JOIN decp_report.file f 
+			ON f.file_id = r.file_id 
+			WHERE r.exclusion_type_id=3
+		 	GROUP BY r.source_id,r.session_id,r.file_id,f.nb_marches,f.nb_concessions
+		 	ORDER BY r.session_id
+		) r
+		GROUP BY r.source_id,r.session_id
+		ORDER BY r.session_id
+	) r
+	INNER JOIN decp_report.source s 
+	ON s.source_id = r.source_id
+	ORDER BY name
+) r
+GROUP BY session_id,session_date
+ORDER BY session_date;
+
+SELECT * FROM decp_report.v_stats_global;
 
