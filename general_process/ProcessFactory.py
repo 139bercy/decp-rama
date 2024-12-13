5b8be59b97dd0d48665b43c8a562344b051c63ee
Your Name <you@example.com>
2024-12-13 17:52:18 +0100

    Restored dematis source

--- general_process/ProcessFactory.py
+++ general_process/ProcessFactory.py
@@ -22,7 +22,6 @@ class ProcessFactory:
         #self.processes = [SampleJsonProcess]
         #self.processes = [SampleXmlProcess]
         self.processes = [Emar2024Process,Pes2024Process]
-        self.processes = [Pes2024Process]
         #self.processes = [Atexo2024Process,Megalis2024Process,Aws2024Process]
         #self.processes = [Megalis2024Process]
         #self.processes = [Emar2024Process]
