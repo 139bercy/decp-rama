30ef9d1c0ce388a59fb362e3ba4ada8dac9feb5f
ECO <emmanuel.couffin@axyus.com>
2026-03-04 11:27:13 +0100

    Added medialex source

--- general_process/ProcessFactory.py
+++ general_process/ProcessFactory.py
@@ -20,7 +20,8 @@ class ProcessFactory:
         #self.processes = [Emar2024Process]
         #self.processes = [SampleJsonProcess]
         #self.processes = [SampleXmlProcess]
-        self.processes = [Emar2024Process,Pes2024Process,Ppsmj2024Process,Xmarches2024Process,Aws2024Process,Modulademat2024Process,Atexo2024Process]
+        self.processes = [Emar2024Process,Pes2024Process,Ppsmj2024Process,Xmarches2024Process,Aws2024Process,Modulademat2024Process,Atexo2024Process,EuroStrasbourgProcess,VilleStrasbourgProcess,Medialex2024Process]
+        #self.processes = [Emar2024Process,Pes2024Process,Ppsmj2024Process,Xmarches2024Process,Aws2024Process,Modulademat2024Process,Atexo2024Process]
         #self.processes = [Proxilegales2024Process,Achatspublicscorse2024Process,Marchespublicspro2024Process,Provencecorse2024Process,Antilleslegales2024Process,Marchedemat2024Process]
         #self.processes = [Atexo2024Process,Megalis2024Process,Aws2024Process]
         #self.processes = [Megalis2024Process]
@@ -36,6 +37,9 @@ class ProcessFactory:
         # if data_format=='2022':
         # self.processes = [SampleXmlProcess] # For test ECO
         #self.processes = [SampleJsonProcess] # For test ECO
+        #self.processes = [DemoApiProcess]
+        #self.processes = [EuroStrasbourgProcess,VilleStrasbourgProcess]
+        #self.processes = [Medialex2024Process]
         self.dataframes = []
         self.data_format = data_format
         self.report = report
